282cbe2b51b8c22292191e2d89bdcd75e79ea363
sarina <sarina.saghari8301@gmail.com>
2022-07-14 12:25:03 +0330

    SinglePage1

---
+++ SinglePageArchitecture/Sample01/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sample01
+{
+    public class Comment
+    {
+        #region [-Step1 Code First -]
+
+        #endregion
+    }
+}
